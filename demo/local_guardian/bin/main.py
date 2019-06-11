e373fa728a42a20c06fb395f6a200edaf89bf2f6
Yi Wang <lovegoods@163.com>
2019-06-11 18:09:04 +0800

    local demo

---
+++ demo/local_guardian/bin/main.py
@@ -0,0 +1,36 @@
+# -*- coding: UTF-8 -*-
+# @Time    : 2018/5/24 下午3:49
+# @File    : main.py
+"""
+guardian demo
+"""
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/' + '../lib/'))
+from ark.assemble.localpull_keymapping import LocalPullKeyMappingGuardian
+from ark.are.executor import BaseExecFuncSet
+from ark.are import log
+
+
+class DemoExecFuncSet(BaseExecFuncSet):
+    """
+    demo exec
+    """
+    def say_hello(self, params):
+        """
+
+        :param params:
+        :return:
+        """
+        log.info("hello, event params:{}".format(params))
+        return {}
+
+
+def guardian_main(mode):
+    """
+
+    :return:
+    """
+    guardian = LocalPullKeyMappingGuardian(
+        "../event", {"hello": "say_hello"}, "strategy", DemoExecFuncSet())
+    return guardian
