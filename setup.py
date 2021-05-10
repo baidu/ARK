ec52128440f79735daf6f8de12a0a419517d26b0
Yi Wang <lovegoods@163.com>
2021-05-10 19:49:23 +0800

    v0.5 update

--- setup.py
+++ setup.py
@@ -8,10 +8,19 @@
 """
 Setup script.
 
-Date:    2018/05/16 14:41:10
+Date:    2018/02/09
 """
-
 import setuptools
 
-setuptools.setup()
+# 当python版本低于2.7.4时，如果其他module在atexit中注册了函数，
+# 那么pbr包的lazy loading将中止setuptools的执行
+# 根据此方案增加以下代码解决该问题: http://bugs.python.org/issue15881#msg170215
+try:
+    import multiprocessing  # noqa
+except ImportError:
+    pass
 
+setuptools.setup(
+    setup_requires=['pbr'],
+    pbr=True,
+)
